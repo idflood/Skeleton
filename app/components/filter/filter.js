ebe223d88550d9670468eeb7fa23dbebf17a8835
David Mignot <david.mig@gmail.com>
2015-04-20 13:39:11 +0200

    start to implement search javascript

--- app/components/filter/filter.js
+++ app/components/filter/filter.js
@@ -1,6 +1,7 @@
 define(['jquery'], function($){
   'use strict';
   var OPEN_CLASS = 'is-open';
+  var $filter = $('.filter');
 
   $('body').on('click.filter', function(e) {
     var $target = $(e.target);
@@ -8,13 +9,33 @@ define(['jquery'], function($){
 
     // If we click on the button we toggle the callout.
     if ($target.hasClass('filter-button')) {
-      $('.filter').toggleClass(OPEN_CLASS);
+      $filter.toggleClass(OPEN_CLASS);
       return;
     }
 
-    // We close the callout if we clicked outside or if we clicked on a item.
-    if (!is_click_inside_callout || $target.hasClass('filter-content-item')) {
-      $('.filter').removeClass(OPEN_CLASS);
+    // We close the callout if we clicked outside.
+    if (!is_click_inside_callout) {
+      $filter.removeClass(OPEN_CLASS);
     }
+  });
+
+  $('body').on('click.filter', '.filter-item', function(e) {
+    e.preventDefault();
+
+    // Close the callout.
+    $filter.removeClass(OPEN_CLASS);
+
+    // Get the value of the selected filter.
+    var selected = $(this).attr('href').replace('#', '');
+    // If we selected "Alle" set the value to false.
+    if (selected == '') {
+      selected = false;
+    }
+
+    // Keep the clicked value in the filter data
+    $filter.data('value', selected);
+
+    // Trigger the custom event.
+    $filter.trigger('filter', [selected]);
   });
 });
--- app/components/search-field/search-field.js
+++ app/components/search-field/search-field.js
@@ -1,6 +1,10 @@
 define(['jquery'], function($){
   'use strict';
+  var HIDDEN_CLASS = 'is-hidden';
   var FOCUS_CLASS = 'is-focused';
+  var $items = $('[data-searchable]');
+  var filter = false;
+  var searchString = '';
 
   $('.search-field')
     .on('focus.search-field', 'input', function(e) {
@@ -8,5 +12,50 @@ define(['jquery'], function($){
     })
     .on('blur.search-field', 'input', function(e) {
       $(this).parent().removeClass(FOCUS_CLASS)
+    })
+    .on('change.search-field, keyup.search-field', 'input', function(e) {
+      // Trim whitespace from start and end of the searchstring.
+      searchString = $.trim($(this).val());
+      console.log("on change", searchString);
+      filterContent();
+    })
+    .on('filter.search-field', '.filter', function(e, filterValue) {
+      filter = filterValue;
+      filterContent();
     });
+
+  var strContains = function(str, search) {
+    // Check for index on lowercased string so that it is case insensitive.
+    return str.toLowerCase().indexOf(search.toLowerCase()) > -1;
+  }
+
+  var filterContent = function() {
+    // If there is no filter and no search string we simply display all items.
+    if (filter === false && searchString == '') {
+      $items.parent().removeClass(HIDDEN_CLASS);
+      return;
+    }
+
+    // Loop over all searchable items.
+    $items.each(function() {
+      var isVisible = true;
+      var $this = $(this);
+
+      console.log(filter);
+      // If there is a filter and the item doesn't match it we hide the item.
+      if (filter && $this.data('filter') != filter) {
+        isVisible = false;
+      }
+
+      // If item is alredy hidden by the filter no need to search for string.
+      if (isVisible) {
+        var text = $this.text();
+        if (searchString != '' && strContains(text, searchString) === false) {
+          isVisible = false;
+        }
+      }
+      // Need to set the visibility class on the parent (the grid-cell).
+      $this.parent().toggleClass(HIDDEN_CLASS, !isVisible);
+    });
+  };
 });
